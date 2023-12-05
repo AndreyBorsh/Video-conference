4262e71e69572ebc4f1a35144e7fc99ddf6e3f7a
AndreyBorsh <nankuzn19@gmail.com>
2023-12-05 20:11:02 +0300

    an empty project

---
+++ src/main/java/com/example/unidubnameet/UnidubnaMeetApplication.java
@@ -0,0 +1,13 @@
+package com.example.unidubnameet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnidubnaMeetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnidubnaMeetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/unidubnameet/UnidubnaMeetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.unidubnameet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnidubnaMeetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
