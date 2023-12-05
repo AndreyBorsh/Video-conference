da0f0c72ba0a43dd3efa08d8a679b29cf9ef7819
AndreyBorsh <nankuzn19@gmail.com>
2023-12-05 20:48:11 +0300

    add register page and login page

---
+++ src/main/java/com/example/unidubnameet/controllers/MainController.java
@@ -0,0 +1,21 @@
+package com.example.unidubnameet.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/register")
+    public String register(Model model) {
+        model.addAttribute("title", "Страница регистрации");
+        return "register";
+    }
+    @GetMapping("/login")
+    public String login(Model model) {
+        model.addAttribute("title", "Страница входа");
+        return "login";
+    }
+}
